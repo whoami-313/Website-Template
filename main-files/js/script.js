07fb0959c7a3936f2e6e72d8a990afe3a42af21a
Cicada 3301 <Cicada.3301_MA_313@yahoo.com>
2023-03-08 22:11:36 +0330

    update commit

---
+++ main-files/js/script.js
@@ -0,0 +1,13 @@
+const barMenu = document.querySelector(".logo-bar-container");
+const menuBar = document.querySelector(".menu");
+
+barMenu.addEventListener("click", function () {
+    if (barMenu.firstElementChild.classList.contains("fa-bars")) {
+        barMenu.firstElementChild.setAttribute("class", "svg-inline--fa fa-xmark");
+        menuBar.style.left = 0;
+    }
+    else {
+        barMenu.firstElementChild.setAttribute("class", "svg-inline--fa fa-bars");
+        menuBar.style.left = "-30rem";
+    }
+});
